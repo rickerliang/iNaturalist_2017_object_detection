cb6e859ebd29173ab796ca3f87910b145df62986
rickerliang <rickerliang@gmail.com>
2018-04-16 10:13:19 +0800

    fix typo

--- eval.sh
+++ eval.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=$PYTHONPATH:/home/lyk/machine_learning/Supervised_Learning/tensorflow_model/research/:/home/lyk/machine_learning/Supervised_Learning/tensorflow_model/research/slim
-CUDA_VISIBLE_DEVICES=1 python object_detection/eval.py --logtostderr --pipeline_config_path=faster_rcnn_resnet101_coco.config --checkpoint_dir==train_dir --eval_dir=eval_dir
+CUDA_VISIBLE_DEVICES=1 python object_detection/eval.py --logtostderr --pipeline_config_path=faster_rcnn_resnet101_coco.config --checkpoint_dir=train_dir --eval_dir=eval_dir
